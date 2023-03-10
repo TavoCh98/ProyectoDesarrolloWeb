23d5b4a3a0fd8196a5e1a5c5e55ac7ae26ed0a92
TavoCh98 <guschaga@gmail.com>
2023-03-10 08:24:27 -0600

    Inicialización del Poryecto

---
+++ src/main/java/com/Proyecto_DesaWeb/ProyectoDesaWebApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto_DesaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDesaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDesaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto_DesaWeb/ProyectoDesaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto_DesaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDesaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
