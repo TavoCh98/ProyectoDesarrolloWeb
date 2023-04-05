28dced36caa12ec4939b6aeefc7f75a101731550
mariafernandamiranda <mariafernandamiranda@localhost>
2023-04-04 21:09:05 -0600

    Service, Dao, Domain, Controller

--- src/main/java/com/Proyecto_DesaWeb/domain/Usuario.java
+++ src/main/java/com/Proyecto_DesaWeb/domain/Usuario.java
@@ -2,6 +2,8 @@
 package com.Proyecto_DesaWeb.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
